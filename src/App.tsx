2208af27cddfe98bf2aeb55295f84e9560893f34
Joanna Bednarczyk <joanna.k.bednarczyk@gmail.com>
2024-07-26 15:49:17 +0200

    Add base for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/global.css';
+import { Navbar } from './components/navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
+      <Navbar />
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +24,7 @@ function App() {
         </a>
       </header>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import{ FC } from "react";
+import './Navbar.css'
+
+export const Navbar: FC = () => {
+    return (
+    <div className="navbar-body">
+        <div className="navbar-logo">
+            Salon Unikatów
+        </div>
+    </div>
+    )
+}

03326676029d6e094a66f82145ebef09516ed86a
Joanna Bednarczyk <joanna.k.bednarczyk@gmail.com>
2024-07-26 21:43:15 +0200

    Add navbar base

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './styles/global.css';
 import { Navbar } from './components/navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -7,25 +6,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <Navbar />
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </Router>
-  );
-}
+     <Navbar />
+     <main className="main-content">
+       <Routes>
+         <Route path="/" element="" />
+         <Route path="/news" element="" />
+         {/* Define other routes that you need*/}
+       </Routes>
+     </main>
+   </Router>
+ );
+};
 
 export default App;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,88 @@
-import{ FC } from "react";
+import{ FC, useState } from "react";
 import './Navbar.css'
+import { NavLink } from "react-router-dom";
+import { IoClose, IoMenu } from "react-icons/io5";
 
 export const Navbar: FC = () => {
-    return (
-    <div className="navbar-body">
-        <div className="navbar-logo">
-            Salon Unikatów
+    const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const closeMenuOnMobile = () => {
+    if (window.innerWidth <= 1150) {
+      setShowMenu(false);
+    }
+  };
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <NavLink to="/" className="nav__logo">
+          Navigation Bar
+        </NavLink>
+
+        <div
+          className={`nav__menu ${showMenu ? "show-menu" : ""}`}
+          id="nav-menu"
+        >
+          <ul className="nav__list">
+            <li className="nav__item">
+              <NavLink to="/" className="nav__link" onClick={closeMenuOnMobile}>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/news"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
+                News
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/about-us"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
+                About Us
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/favorite"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
+                Favorite
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/location"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
+                Location
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/get-started" className="nav__link nav__cta">
+                Get Started
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav__close" id="nav-close" onClick={toggleMenu}>
+            <IoClose />
+          </div>
         </div>
-    </div>
-    )
-}
+
+        <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+          <IoMenu />
+        </div>
+      </nav>
+    </header>
+  );
+};

31190fdd66083d31dd39b473ae8083def43b8fb5
Joanna Bednarczyk <joanna.k.bednarczyk@gmail.com>
2024-07-29 22:08:18 +0200

    Add image in second section

--- src/components/home/HomePage.component.tsx
+++ src/components/home/HomePage.component.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './HomePage.css';
+import Clock from '../../assets/images/clock.png';
 
 export const HomePage = () => {
     return <>
@@ -9,10 +10,13 @@ export const HomePage = () => {
                 <p>Z Nami Odkryjesz Piękno Przeszłości</p>
             </div>
         </section>
-        <div className="introduction">
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut erat in purus accumsan placerat. Vestibulum imperdiet ornare turpis, non semper sapien commodo nec. Quisque dolor mauris, euismod vitae sagittis et, sodales id sem. Sed et convallis felis. Fusce eu pretium ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae urna magna. Morbi scelerisque nisl dapibus mollis tristique. Mauris ullamcorper felis nunc, non mattis lacus condimentum ac. Phasellus at augue et arcu sagittis consequat sed sed leo.
-            </p>
-        </div>
+        <section className="introduction">
+            <div className="intro-content">
+            <img src={Clock} alt="" className="intro-image" />
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut erat in purus accumsan placerat. Vestibulum imperdiet ornare turpis, non semper sapien commodo nec. Quisque dolor mauris, euismod vitae sagittis et, sodales id sem. Sed et convallis felis. Fusce eu pretium ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae urna magna. Morbi scelerisque nisl dapibus mollis tristique. Mauris ullamcorper felis nunc, non mattis lacus condimentum ac. Phasellus at augue et arcu sagittis consequat sed sed leo.
+                </p>
+            </div>
+        </section>
     </>
 };

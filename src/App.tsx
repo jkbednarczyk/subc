0a005de541ea5dfe985b7958b25ff8328d3d4314
Joanna Bednarczyk <joanna.k.bednarczyk@gmail.com>
2024-07-29 12:42:12 +0200

    Add background image

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import './GlobalStyles.css';
 import { Navbar } from './components/navbar/Navbar.component';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HomePage } from './components/home/HomePage.component';
 
 function App() {
   return (
+    
     <Router>
      <Navbar />
      <main className="main-content">
-      <div className='nova'>
-        TEXT
-      </div>
        <Routes>
-         <Route path="/" element="" />
+         <Route path="/" element={<HomePage />} />
          <Route path="/news" element="" />
          {/* Define other routes that you need*/}
        </Routes>
      </main>
    </Router>
+  // <HomePage/>
  );
 };
 
---
+++ src/components/home/HomePage.component.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './HomePage.css';
+
+export const HomePage = () => {
+    return <>
+        <section className="home">
+            <div className="content">
+                <h1>Salon Unikatów</h1>
+                <p>Z Nami Odkryjesz Piękno Przeszłości</p>
+            </div>
+        </section>
+        <div className="introduction">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut erat in purus accumsan placerat. Vestibulum imperdiet ornare turpis, non semper sapien commodo nec. Quisque dolor mauris, euismod vitae sagittis et, sodales id sem. Sed et convallis felis. Fusce eu pretium ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae urna magna. Morbi scelerisque nisl dapibus mollis tristique. Mauris ullamcorper felis nunc, non mattis lacus condimentum ac. Phasellus at augue et arcu sagittis consequat sed sed leo.
+            </p>
+        </div>
+    </>
+};

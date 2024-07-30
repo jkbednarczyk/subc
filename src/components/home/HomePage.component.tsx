d7d6fa8ff8cdce2e12bbcb89701892e436ea61f2
Joanna Bednarczyk <joanna.k.bednarczyk@gmail.com>
2024-07-30 13:07:00 +0200

    Add fontawesome icons

--- src/components/home/HomePage.component.tsx
+++ src/components/home/HomePage.component.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import './HomePage.css';
 import Clock from '../../assets/images/clock.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+import '../../fontawesome.config';
 
 export const HomePage = () => {
     return <>
@@ -16,6 +19,11 @@ export const HomePage = () => {
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut erat in purus accumsan placerat. Vestibulum imperdiet ornare turpis, non semper sapien commodo nec. Quisque dolor mauris, euismod vitae sagittis et, sodales id sem. Sed et convallis felis. Fusce eu pretium ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae urna magna. Morbi scelerisque nisl dapibus mollis tristique. Mauris ullamcorper felis nunc, non mattis lacus condimentum ac. Phasellus at augue et arcu sagittis consequat sed sed leo.
                 </p>
+            </div>
+        </section>
+        <section className="footer">
+            <div className="footer-content">
+            <FontAwesomeIcon icon={faCopyright} />
             </div>
         </section>
     </>

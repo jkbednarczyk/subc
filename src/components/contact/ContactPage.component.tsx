e969f43aa9199b1c6d2b0cae31ec8c36f9e3e0a3
Joanna Bednarczyk <joanna.k.bednarczyk@gmail.com>
2024-08-06 11:33:47 +0200

    SUBC-0002 Style contact page

--- src/components/contact/ContactPage.component.tsx
+++ src/components/contact/ContactPage.component.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import './ContactPage.css';
 import { ContactForm } from "./contactForm/ContactForm.component";
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope} from '@fortawesome/free-regular-svg-icons';
+import { faLocationDot, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export const ContactPage = () => { 
 
     return <>
         <section className="contact-form">
             <ul className="contact-data">
-            <div className="contact-item">
+            <div className="contact-item contact-intro">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a condimentum augue. Praesent lacinia accumsan dolor, nec semper diam luctus ac. Nullam tincidunt auctor interdum. Etiam pretium tempor felis at hendrerit. Praesent aliquam mi ac massa fringilla mollis. Nunc bibendum erat in enim ultricies, sodales ultrices erat ornare. Vestibulum volutpat, nisi vel tristique pharetra, quam lectus dictum justo, quis porta quam orci vel nisl. Nunc id posuere metus. Sed in pharetra neque. Nam id efficitur arcu. Sed venenatis, ligula vitae bibendum commodo, leo diam scelerisque purus, viverra ornare nisi magna at lorem. Morbi egestas lorem est, eu hendrerit lectus tristique id. Pellentesque tempus massa sed hendrerit facilisis. 
                 </div>
                 <div className="contact-item">
@@ -17,18 +18,30 @@ export const ContactPage = () => {
                         <FontAwesomeIcon icon={faLocationDot}/>
                     </div>    
                     <div className="data">
-                        <p>Salon Unikatów</p>
-                        <p>NIP 1234567890</p>
-                        <p>REGON 12345678901234</p>
-                        <p>ul. Dekarzy 11</p>
-                        <p>30-414 Kraków</p>
+                        <p> 
+                            Salon Unikatów<br/>
+                            NIP 6793272126<br/>
+                            REGON 525857837<br/>
+                            ul. Dekarzy 11<br/>
+                            30-414 Kraków
+                        </p>
                     </div>
                 </div>
                 <div className="contact-item">
-                    salonunikatow@gmail.com
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faEnvelope}/>
+                    </div>    
+                    <div className="data">
+                        salonunikatow@gmail.com
+                    </div>
                 </div>
                 <div className="contact-item">
-                    +48 662 921 155
+                    <div className="icon">
+                        <FontAwesomeIcon icon={faPhone}/>
+                    </div>    
+                    <div className="data">
+                        +48 662 921 155
+                    </div>
                 </div>
             </ul>
             <ul className="contact-data">

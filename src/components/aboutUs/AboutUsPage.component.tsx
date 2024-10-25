d3fa530e0cc9d4cb487515c72cd7391f3238b0ed
Joanna Bednarczyk <joanna.k.bednarczyk@gmail.com>
2024-10-25 12:19:15 +0200

    SUBC-0018 Add Galleria with comments

--- src/components/aboutUs/AboutUsPage.component.tsx
+++ src/components/aboutUs/AboutUsPage.component.tsx
@@ -1,7 +1,39 @@
 import './AboutUsPage.css';
 import { translate } from '../../lang/utils/TranslationUtils';
+import {Galleria, GalleriaResponsiveOptions} from 'primereact/galleria';
+import { useState, useEffect } from 'react';
+import { PhotoService } from './PhotoService.service';
+import {Button} from 'primereact/button';
 
 export const AboutUsPage = () => {
+    const [images, setImages] = useState([]);
+    const responsiveOptions: GalleriaResponsiveOptions[] = [
+        {
+            breakpoint: '991px',
+            numVisible: 4
+        },
+        {
+            breakpoint: '767px',
+            numVisible: 3
+        },
+        {
+            breakpoint: '575px',
+            numVisible: 1
+        }
+    ];
+
+    useEffect(() => {
+        PhotoService.getImages().then((data: any) => setImages(data));
+    }, []);
+
+    const itemTemplate = (item: any) => {
+        return <img src={item.itemImageSrc} alt={item.alt} style={{ width: '100%' }} />
+    }
+
+    const thumbnailTemplate = (item: any) => {
+        return <img src={item.thumbnailImageSrc} alt={item.alt} />
+    }
+
     return <>
         <section className="about_us">
             <div className="about_us_content">
@@ -10,6 +42,10 @@ export const AboutUsPage = () => {
                 <p>{translate("ABOUT_US_PAGE.APPROACH")}</p>
                 <p>{translate("ABOUT_US_PAGE.INVITATION")}</p>
             </div>
+            <div className="about_us_comments">
+            <Galleria value={images} responsiveOptions={responsiveOptions} numVisible={3}  
+                item={itemTemplate} thumbnail={thumbnailTemplate} />
+        </div>
         </section>
     </>
 }

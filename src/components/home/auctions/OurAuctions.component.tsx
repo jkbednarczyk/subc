de4c01e05e132db9b71c07798bf681cb074a26ee
Joanna Bednarczyk <joanna.k.bednarczyk@gmail.com>
2025-02-13 21:35:50 +0100

    SUBC-0031 Move auction tile to common components as link tile

--- src/components/home/auctions/OurAuctions.component.tsx
+++ src/components/home/auctions/OurAuctions.component.tsx
@@ -1,4 +1,4 @@
-import { AuctionsTile } from "./auctionsTile/AuctionsTile.component";
+import { LinkTile } from "../../common/linkTile/LinkTile.component";
 import "./OurAuctions.css";
 import Teacups from '../../../assets/images/teacups.jpg';
 import Meissen from '../../../assets/images/meissen.jpg';
@@ -14,42 +14,42 @@ export const OurAuctions = () => {
     return <div className="auctions">
         <h1>{translate("OUR_OFFER")}</h1>
         <div className="auctions_container">
-            <div className="auctions_wrapper">
+            <div className="links_wrapper">
             <p>{translate("OUR_OFFER_TEXT")}</p>
-                <ul className="auctions_tiles">
-                    <AuctionsTile 
+                <ul className="links_tiles">
+                    <LinkTile 
                         to = {EXTERNAL_LINKS.ALLEGRO_FIGURINES}
                         imageLocation = {Figurines}
                         text = {translate("FIGURINES")}
                         alt = {translate("ALT.FIGURINES")}
                     />
-                    <AuctionsTile 
+                    <LinkTile 
                         to = {EXTERNAL_LINKS.ALLERGO_ROSENTHAL} 
                         imageLocation = {Rosenthal}
                         text = {translate("ROSENTHAL")}
                         alt = {translate("ALT.ROSENTHAL")}
                     />
-                    <AuctionsTile 
+                    <LinkTile 
                         to = {EXTERNAL_LINKS.ALLEGRO_CLOCKS_AND_WATCHES} 
                         imageLocation = {Watches}
                         text = {translate("CLOCKS_AND_WATCHES")}
                         alt = {translate("ALT.CLOCK")}
                     />
                 </ul>
-                <ul className="auctions_tiles">
-                    <AuctionsTile 
+                <ul className="links_tiles">
+                    <LinkTile 
                         to = {EXTERNAL_LINKS.ALLEGRO_MEISSEN} 
                         imageLocation = {Meissen}
                         text = {translate("MEISSEN")}
                         alt = {translate("ALT.MEISSEN")}
                     />
-                    <AuctionsTile 
+                    <LinkTile 
                         to = {EXTERNAL_LINKS.ALLEGRO_CUPS}
                         imageLocation = {Teacups}
                         text = {translate("CUPS")}
                         alt = {translate("ALT.TEACUPS")}
                     />
-                    <AuctionsTile 
+                    <LinkTile 
                         to = {EXTERNAL_LINKS.ALLEGRO_ALL_AUCTIONS} 
                         imageLocation = {Logo}
                         text = {translate("ALL_AUCTIONS")}

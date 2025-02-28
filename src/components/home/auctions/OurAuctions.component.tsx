4b0cd41cb7046881481e8aa9468a89a1fbd0350a
Joanna Bednarczyk <joanna.k.bednarczyk@gmail.com>
2025-02-28 21:03:42 +0100

    SUBC-0028 Add animation for our auctions on main page

--- src/components/home/auctions/OurAuctions.component.tsx
+++ src/components/home/auctions/OurAuctions.component.tsx
@@ -1,62 +1,56 @@
 import { LinkTile } from "../../common/linkTile/LinkTile.component";
 import "./OurAuctions.css";
-import Teacups from '../../../assets/images/teacups.jpg';
-import Meissen from '../../../assets/images/meissen.jpg';
-import Rosenthal from '../../../assets/images/rosenthal.jpg';
-import Watches from '../../../assets/images/clock.jpg';
-import Figurines from '../../../assets/images/figurines.jpg';
-import Logo from '../../../assets/images/logo_redirect.png';
 import { EXTERNAL_LINKS } from "../../common/utils/externalLinks";
-import {translate} from '../../../lang/utils/TranslationUtils';
+import { translate } from "../../../lang/utils/TranslationUtils";
+import { useEffect, useState } from "react";
+import Teacups from "../../../assets/images/teacups.jpg";
+import Meissen from "../../../assets/images/meissen.jpg";
+import Rosenthal from "../../../assets/images/rosenthal.jpg";
+import Watches from "../../../assets/images/clock.jpg";
+import Figurines from "../../../assets/images/figurines.jpg";
+import Logo from "../../../assets/images/logo_redirect.png";
 
+const AUCTION_ITEMS = [
+  { to: EXTERNAL_LINKS.ALLEGRO_FIGURINES, image: Figurines, text: "FIGURINES", alt: "ALT.FIGURINES" },
+  { to: EXTERNAL_LINKS.ALLERGO_ROSENTHAL, image: Rosenthal, text: "ROSENTHAL", alt: "ALT.ROSENTHAL" },
+  { to: EXTERNAL_LINKS.ALLEGRO_CLOCKS_AND_WATCHES, image: Watches, text: "CLOCKS_AND_WATCHES", alt: "ALT.CLOCK" },
+  { to: EXTERNAL_LINKS.ALLEGRO_MEISSEN, image: Meissen, text: "MEISSEN", alt: "ALT.MEISSEN" },
+  { to: EXTERNAL_LINKS.ALLEGRO_CUPS, image: Teacups, text: "CUPS", alt: "ALT.TEACUPS" },
+  { to: EXTERNAL_LINKS.ALLEGRO_ALL_AUCTIONS, image: Logo, text: "ALL_AUCTIONS", alt: "ALT.LOGO_REDIRECT" },
+];
 
 export const OurAuctions = () => {
-    return <div className="auctions">
-        <h1>{translate("OUR_OFFER")}</h1>
-        <div className="auctions_container">
-            <div className="links_wrapper">
-            <p>{translate("OUR_OFFER_TEXT")}</p>
-                <ul className="links_tiles">
-                    <LinkTile 
-                        to = {EXTERNAL_LINKS.ALLEGRO_FIGURINES}
-                        imageLocation = {Figurines}
-                        text = {translate("FIGURINES")}
-                        alt = {translate("ALT.FIGURINES")}
-                    />
-                    <LinkTile 
-                        to = {EXTERNAL_LINKS.ALLERGO_ROSENTHAL} 
-                        imageLocation = {Rosenthal}
-                        text = {translate("ROSENTHAL")}
-                        alt = {translate("ALT.ROSENTHAL")}
-                    />
-                    <LinkTile 
-                        to = {EXTERNAL_LINKS.ALLEGRO_CLOCKS_AND_WATCHES} 
-                        imageLocation = {Watches}
-                        text = {translate("CLOCKS_AND_WATCHES")}
-                        alt = {translate("ALT.CLOCK")}
-                    />
-                </ul>
-                <ul className="links_tiles">
-                    <LinkTile 
-                        to = {EXTERNAL_LINKS.ALLEGRO_MEISSEN} 
-                        imageLocation = {Meissen}
-                        text = {translate("MEISSEN")}
-                        alt = {translate("ALT.MEISSEN")}
-                    />
-                    <LinkTile 
-                        to = {EXTERNAL_LINKS.ALLEGRO_CUPS}
-                        imageLocation = {Teacups}
-                        text = {translate("CUPS")}
-                        alt = {translate("ALT.TEACUPS")}
-                    />
-                    <LinkTile 
-                        to = {EXTERNAL_LINKS.ALLEGRO_ALL_AUCTIONS} 
-                        imageLocation = {Logo}
-                        text = {translate("ALL_AUCTIONS")}
-                        alt = {translate("ALT.LOGO_REDIRECT")}
-                    />
-                </ul>
-            </div>
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 100) setAnimate(true);
+    };
+
+    window.addEventListener("scroll", onScroll);
+    
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  return (
+    <div className={`our-auctions ${animate ? "fade-in" : ""}`}>
+      <h1>{translate("OUR_OFFER")}</h1>
+      <div className = "auctions_container">
+        <div className = "links_wrapper">
+          <p>{translate("OUR_OFFER_TEXT")}</p>
+          <ul className = "links_tiles">
+            {AUCTION_ITEMS.map((item, index) => (
+              <li key = {index} className = "fade-item">
+                <LinkTile 
+                  to = {item.to} 
+                  imageLocation = {item.image} 
+                  text = {translate(item.text)} 
+                  alt = {translate(item.alt)} />
+              </li>
+            ))}
+          </ul>
         </div>
+      </div>
     </div>
-}
+  );
+};

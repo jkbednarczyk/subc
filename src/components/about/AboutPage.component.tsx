93fa371cc0f2a18857290df72f193f51162f561a
Joanna Bednarczyk <joanna.k.bednarczyk@gmail.com>
2025-05-06 21:42:57 +0200

    SUBC-0036 Add translations

--- src/components/about/AboutPage.component.tsx
+++ src/components/about/AboutPage.component.tsx
@@ -6,6 +6,7 @@ import Napoleon from "../../assets/images/napoleon.jpg"
 import { IconTile } from "../common/iconTile/IconTIle.component";
 import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 import { FaDolly, FaMagnifyingGlass, FaHandshake } from "react-icons/fa6";
+import { translate } from "../../lang/utils/TranslationUtils";
 
 export const AboutPage = () => {
     return <>
@@ -16,40 +17,40 @@ export const AboutPage = () => {
                 content = {METADATA.ABOUT_DESCRIPTION} />
         </Helmet>
         <section className = "about_hero">
-            <h1>Nasza historia i pasja</h1>
-            <h2>Piękno przeszłości w sercu współczesności.</h2>
+            <h1>{translate("ABOUT_PAGE.TITLE")}</h1>
+            <h2>{translate("ABOUT_PAGE.SUBTITLE")}</h2>
         </section>
         <section className = "about">
             <div className = "about__content-wrapper">
                 <div className = "about__text-container">
-                    <h3>Nasza misja</h3>
-                    <p>Salon Unikatów to rodzinna inicjatywa stworzona z pasji do piękna, historii i przedmiotów z duszą. Wierzymy, że antyki to nie tylko dekoracje – to opowieści, wspomnienia i wyjątkowy klimat, który mogą Państwo zaprosić do swojego domu. </p>
-                    <p>Chcemy dzielić się tą pasją, oferując Państwu unikatowe przedmioty, które łączą estetykę z historią. Naszym celem jest, aby każdy klient poczuł, że otrzymuje coś więcej niż tylko przedmiot – część dziedzictwa.</p>
-                    <h3>Skąd pochodzą nasze unikaty</h3>
-                    <p>Nasze kolekcje powstają dzięki starannej selekcji. Odwiedzamy giełdy staroci, domy aukcyjne i prywatne kolekcje w całej Europie, by odnaleźć prawdziwe perełki.</p>
-                    <p>Każdy przedmiot trafiający do naszego salonu przechodzi dokładną weryfikację pod względem autentyczności, jakości i stanu zachowania. Tylko wtedy trafia do naszej oferty – z myślą o tym, by stał się częścią Państwa kolekcji.</p>
-                    <h3>Co nas wyróżnia</h3>
-                    <p>Jesteśmy małą firmą i to właśnie daje nam przewagę. Zapewniamy indywidualne podejście do każdego klienta, szybki kontakt i pełne zaangażowanie w realizację zamówień.</p>
-                    <p>Dbamy o bezpieczeństwo zakupów: każdy przedmiot pakujemy z najwyższą starannością, by dotarł w nienaruszonym stanie. Zadowolenie klientów jest dla nas najważniejsze – to ono motywuje nas do dalszego działania.</p>
+                    <h3>{translate("ABOUT_PAGE.MISSION_TITLE")}</h3>
+                    <p>{translate("ABOUT_PAGE.MISSION_1")}</p>
+                    <p>{translate("ABOUT_PAGE.MISSION_2")}</p>
+                    <h3>{translate("ABOUT_PAGE.COLLECTION_ORIGIN_TITLE")}</h3>
+                    <p>{translate("ABOUT_PAGE.COLLECTION_ORIGIN_1")}</p>
+                    <p>{translate("ABOUT_PAGE.COLLECTION_ORIGIN_2")}</p>
+                    <h3>{translate("ABOUT_PAGE.CHARACTERISTICS_TITLE")}</h3>
+                    <p>{translate("ABOUT_PAGE.CHARACTERISTICS_1")}</p>
+                    <p>{translate("ABOUT_PAGE.CHARACTERISTICS_2")}</p>
                 </div>
                 <div className = "about__image-container">
                     <img src = {Napoleon} alt = "About Us" className = "about__image" />  
-                    <p>"Każdy przedmiot ma swoją historię. My pomagamy ją opowiedzieć."</p> 
+                    <p>{translate("ABOUT_PAGE.QUOTE")}</p> 
                 </div>
             </div>
             <div className = "about__tiles-container">
                 <IconTile 
                     icon = {<FaMagnifyingGlass/>} 
-                    title = {"Autentyczność"} />
+                    title = {translate("ABOUT_PAGE.TILE_1")} />
                 <IconTile 
                     icon = {<FaDolly/>} 
-                    title = {"Bezpieczne pakowanie"} />
+                    title = {translate("ABOUT_PAGE.TILE_2")} />
                 <IconTile 
                     icon = {<FaHandshake/>} 
-                    title = {"Bezpośredni kontakt"} /> 
+                    title = {translate("ABOUT_PAGE.TILE_3")} /> 
                 <IconTile 
                     icon = {<IoMdCheckmarkCircleOutline/>} 
-                    title = {"Zaufanie kolekcjonerów"} />            
+                    title = {translate("ABOUT_PAGE.TILE_4")} />            
             </div>
         </section>
         <Footer/>

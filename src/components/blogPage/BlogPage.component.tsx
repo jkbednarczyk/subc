fad89ded06eafee353bacfb5d01664a44a041552
Joanna Bednarczyk <joanna.k.bednarczyk@gmail.com>
2025-05-16 21:29:53 +0200

    SUBC-0039 Add BlogPage component with hero section

--- src/components/blogPage/BlogPage.component.tsx
+++ src/components/blogPage/BlogPage.component.tsx
@@ -1,10 +1,21 @@
+import { Helmet } from 'react-helmet-async';
+import { METADATA } from '../common/utils/metadata';
 import './BlogPage.css';
 
+
 export const BlogPage = () => {
-  return (
-    <div className="blog-page">
-      <h1>Blog Page</h1>
-      <p>This is the blog page content.</p>
-    </div>
-  );
+  
+
+  return <>
+    <Helmet>
+      <title>ADD TEXT AND METADATA</title>
+      <meta 
+        name="description" 
+        content={METADATA.ABOUT_DESCRIPTION} />
+    </Helmet>
+    <section className="blog_hero">
+      <h1>Z miłości do rzeczy pięknych i niepowtarzalnych</h1>
+      <h2>Blog o sztuce kolekcjonowania i pasji do antyków.</h2>
+    </section>
+  </>
 }

a927037542800444bb6bdbb95dce5b9973990073
Joanna Bednarczyk <joanna.k.bednarczyk@gmail.com>
2025-05-20 09:32:36 +0200

    SUBC-0039 Add paginator from prime react

--- src/components/blogPage/blogList/BlogList.component.tsx
+++ src/components/blogPage/blogList/BlogList.component.tsx
@@ -1,26 +1,20 @@
 import { FC, useState } from "react";
 import { BlogPostSummary } from "../BlogPage.utils";
-import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 import './BlogList.css';
+import { Paginator } from "primereact/paginator";
 
 interface BlogListProps {
   posts: BlogPostSummary[];
 }
 
-const POSTS_PER_PAGE: number = 2;
+const POSTS_PER_PAGE: number = 6;
 
 export const BlogList: FC<BlogListProps> = ({ posts }) => {
     const [currentPage, setCurrentPage] = useState(1);
-
-    const totalPages:number = Math.ceil(posts.length / POSTS_PER_PAGE);
+    const [first, setFirst] = useState(0);
 
     const startIndex: number = (currentPage - 1) * POSTS_PER_PAGE;
     const currentPosts = posts.slice(startIndex, startIndex + POSTS_PER_PAGE);
-
-    const handlePageChange = (page: number) => {
-        setCurrentPage(page);
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
 
     return (
         <>
@@ -34,26 +28,17 @@ export const BlogList: FC<BlogListProps> = ({ posts }) => {
                     </article>
                 ))}
             </section>
-            <div className="pagination">
-                <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
-                    <FaChevronLeft />
-                </button>
-
-                {Array.from({ length: totalPages }, (_, index) => (
-                    <button
-                        key={index + 1}
-                        onClick={() => handlePageChange(index + 1)}
-                        className={currentPage === index + 1 ? 'active' : ''}>
-                            {index + 1}
-                    </button>
-            ))}
-
-                <button 
-                    onClick={() => handlePageChange(currentPage + 1)}  
-                    disabled={currentPage === totalPages}>
-                        <FaChevronRight />
-                </button>
-            </div>
+            <Paginator
+                first={first}
+                rows={POSTS_PER_PAGE}
+                totalRecords={posts.length}
+                onPageChange={(e) => {
+                    setFirst(e.first);
+                    setCurrentPage(e.page + 1);
+                    window.scrollTo({ top: 0, behavior: 'smooth' });
+                }}
+                alwaysShow={false}
+            />
         </>
     );
 };
